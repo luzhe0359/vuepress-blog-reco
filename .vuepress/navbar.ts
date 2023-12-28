7ad576c5703835f3ae0f5bd634e41e5d734bd16b
luzhe <luzhe@linewell.com>
2023-12-28 18:01:48 +0800

    blog 写博客

--- .vuepress/navbar.ts
+++ .vuepress/navbar.ts
@@ -1,9 +1,9 @@
 // 导航栏配置
 export default [
-  { text: '分类', link: '/categories/blog/1/' },
-  { text: '标签', link: '/tags/blog/1/' },
+  { text: '分类', link: '/categories/quweishenghuo/1/' },
+  { text: '标签', link: '/tags/richang/1/' },
   { text: '收藏', link: '/docs/collection/001' },
-  { text: '友联', link: '/friendship-link' },
+  { text: '友链', link: '/friendship-link' },
   { text: '时间轴', link: '/timeline' },
   {
     text: '关于',
